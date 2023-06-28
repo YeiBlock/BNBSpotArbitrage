e1c3bf7ade071f80c379916cb0d5c72e6e408077
YeiBlock <115025076+YeiBlock@users.noreply.github.com>
2023-06-28 13:11:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ const server = app.listen(8080, '0.0.0.0', () =>
   log('Arbitrage Bot has just started on port 8080. Please wait.....')
 );
 
+// Set up CORS and static file serving middleware
 app.use(cors());
 app.use('/JS', express.static(path.join(__dirname, './Pages/JS')));
+
+// Serve the index.html file
 app.get('/', (_, res) => {
   res.sendFile(path.join(__dirname, './Pages/index.html'));
 });
 
-const io = socket(server);
+// Create the socket.io instance
+const io = socket(server, {
+  // Only allow IPv4 connections
+  ipv6: false,
+});
 
 const arbitrage = require('./arbitrage');
 
